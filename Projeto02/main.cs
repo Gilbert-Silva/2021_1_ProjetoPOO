d528f7a976c57eb33107b9f15275bc6db756a72d
GilbertAzevedo <gilbertif1995@outlook.com>
2021-07-15 00:57:22 +0000

    Associação entre Produto e Categoria

---
+++ Projeto02/main.cs
@@ -0,0 +1,19 @@
+using System;
+
+class MainClass {
+  public static void Main() {
+    Categoria c1 = new Categoria(1, "Mouse");
+    Categoria c2 = new Categoria(2, "Fonte");
+    Console.WriteLine(c1);
+    Console.WriteLine(c2);
+
+    Produto p1 = new Produto(1, "Mouse Bluetooth", 10, 150, c1);
+    Produto p2 = new Produto(2, "Mouse Usb", 15, 60, c1);
+    Produto p3 = new Produto(3, "Fonte Atx", 20, 300, c2);
+    Produto p4 = new Produto(4, "Teclado Usb", 20, 100);    
+    Console.WriteLine(p1);
+    Console.WriteLine(p2);
+    Console.WriteLine(p3);
+    Console.WriteLine(p4);
+  }
+}
